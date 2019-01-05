0f389b1336d6f3fbd82dd235e070409bd35990ed
Alexey Kulakov <uni_que@me.com>
2019-01-04 18:18:13 -0800

    minor clean up and deps bump

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -4,10 +4,6 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 const App = Application.extend({
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
